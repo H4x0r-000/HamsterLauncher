431387cb684bcdfe3da36af69d074eb2af946d1f
H4x0r-000 <76412429+H4x0r-000@users.noreply.github.com>
2022-08-22 03:42:10 +0200

    kek

--- src/MainForm.cpp
+++ src/MainForm.cpp
@@ -4,12 +4,13 @@ wxBEGIN_EVENT_TABLE(MainForm, wxFrame)
 
 wxEND_EVENT_TABLE()
 
-MainForm::MainForm() : wxFrame(nullptr, wxID_ANY, "Test program")
+MainForm::MainForm() : wxFrame(nullptr, wxID_ANY, "Hamster Launcher Pre-Alpha 0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.1")
 {
-
+	wxButton* button1 = new wxButton(this, wxID_ANY, "Einz Coolen Button", wxPoint(50, 50), wxSize(300, 50));
 }
 
 MainForm::~MainForm()
 {
 
 }
+ 
